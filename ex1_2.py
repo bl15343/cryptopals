83c75a8c457c2d0553fe4da1a4ddac757b0e4e4a
Brian Lucas <lucas@quorum.us>
2019-09-21 11:13:35 -0400

    Use binascii.hexlify to convert bytearray to hex string

--- ex1_2.py
+++ ex1_2.py
@@ -1,5 +1,7 @@
 #Fixed XOR
 
+import binascii
+
 def fixed_xor(buf1, buf2):
     bin_buf1 = bytearray.fromhex(buf1)
     bin_buf2 = bytearray.fromhex(buf2)
@@ -9,6 +11,6 @@ def fixed_xor(buf1, buf2):
     for i in range(len(bin_buf1)):
         result_buf[i] = bin_buf1[i] ^ bin_buf2[i]
 
-    return bytes(result_buf).hex()
+    return binascii.hexlify(result_buf)
 
 print('Result from fixed_xor', fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
